85d153ca189b8af7c6f88fc0f1a7c92cd047478c
wangjingru <w15673142360@gmail.com>
2019-07-02 14:11:26 +0800

    Set directory file

---
+++ components/home/Home.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="">
+    Home Header
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
